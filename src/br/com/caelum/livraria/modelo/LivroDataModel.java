063ec183721a3736fec07065169bd4e4125ee212
marcosdenisalves <marcos.denisjr@gmail.com>
2020-11-01 13:11:19 -0300

    instanciando objeto dao

--- src/br/com/caelum/livraria/modelo/LivroDataModel.java
+++ src/br/com/caelum/livraria/modelo/LivroDataModel.java
@@ -12,7 +12,7 @@ public class LivroDataModel extends LazyDataModel<Livro> {
 
 	private static final long serialVersionUID = 1L;
 	
-	private DAO<Livro> dao;
+	private DAO<Livro> dao = new DAO<Livro>(Livro.class);
 	
 	public LivroDataModel() {
 		super.setRowCount(dao.quantidadeDeElementos());
@@ -20,7 +20,8 @@ public class LivroDataModel extends LazyDataModel<Livro> {
 	
 	@Override
 	public List<Livro> load(int inicio, int quantidade, String campoOrdenacao, SortOrder sentidoOrdenacao, Map<String, Object> filtros) {
-	    return dao.listaTodosPaginada(inicio, quantidade);
+	    String titulo = (String) filtros.get("titulo");
+		return dao.listaTodosPaginada(inicio, quantidade);
 	}
 
 }
